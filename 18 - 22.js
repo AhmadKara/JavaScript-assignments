4561ab3aec46885b2225831e95961bbb33d857c0
Ahmad <86618117+AhmadKara@users.noreply.github.com>
2022-07-10 18:04:30 +0300

    Create 18 - 22.js

---
+++ 18 - 22.js
@@ -0,0 +1,27 @@
+let points = 10;
+
+// Write Your Code Here
+++points
+++points
+++points
+console.log(points); // 13
+
+// Write Your Code Here
+--points
+--points
+--points
+--points
+--points
+console.log(points); // 8;
+
+
+
+// The Challenge
+
+let d = "-100";
+let e = "20";
+let f = 30;
+let g = true;
+
+console.log(-d * e); // 2000
+console.log(-d + -d + ++g - --f); // 173
