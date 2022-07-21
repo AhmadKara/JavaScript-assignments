9b59a307b3924c9c2f34b024159b0e5e7b21231d
Ahmad <86618117+AhmadKara@users.noreply.github.com>
2022-07-21 18:59:52 +0300

    Create 57-63.js

---
+++ 57-63.js
@@ -0,0 +1,50 @@
+function sayHello(theName, theGender) {
+  if (theGender === "Male") {
+    console.log(`Hello Mr ${theName}`);
+  } else if (theGender === "Female") {
+    console.log(`Hello Miss ${theName}`);
+  } else {
+    console.log(`Hello ${theName}`);
+  }
+}
+
+// Needed Output
+sayHello("Osama", "Male"); // "Hello Mr Osama"
+sayHello("Eman", "Female"); // "Hello Miss Eman"
+sayHello("Sameh"); // "Hello Sameh"
+
+//////////////////////////
+
+function calculate(firstNum, secondNum, operation) {
+  if (secondNum === undefined) {
+    console.log("Second Number Not Found");
+  } else if (operation === undefined || operation === "add") {
+    console.log(firstNum + secondNum);
+  } else if (operation === "subtract") {
+    console.log(firstNum - secondNum);
+  } else if (operation === "multiply") {
+    console.log(firstNum * secondNum);
+  }
+}
+
+// Needed Output
+calculate(20); // Second Number Not Found
+calculate(20, 30); // 50
+calculate(20, 30, "add"); // 50
+calculate(20, 30, "subtract"); // -10
+calculate(20, 30, "multiply"); // 600
+
+//////////////////////////////////////
+
+
+function ageInTime(theAge) {
+  if (theAge < 10 || theAge > 100) {
+    console.log("Age Out Of Range")
+  } else {
+    console.log(`Your Age In Months => ${theAge * 12} Momths`)
+  }
+}
+
+// Needed Output
+ageInTime(110); // Age Out Of Range
+ageInTime(38); // Months Example => 456 Months
