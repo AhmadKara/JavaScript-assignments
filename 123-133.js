9172971d8f59c98208f22f37b3c5588d179edf28
Ahmad <86618117+AhmadKara@users.noreply.github.com>
2022-08-18 16:14:20 +0300

    Update 123-133.js

--- 123-133.js
+++ 123-133.js
@@ -6,7 +6,7 @@ console.log(setOfNumbers);
 // Set(3) {10, 20, 2}
 // 2
 
-//////////////////////
+//////////////////////////
 
 let myFriends = ["Osama", "Ahmed", "Sayed", "Sayed", "Mahmoud", "Osama"];
 let myFriendsSort = Array.from(new Set(myFriends.sort()));
@@ -24,11 +24,68 @@ let myInfo = {
 };
 
 let myMap = new Map(Object.entries(myInfo));
-console.log(myMap)
-console.log(myMap.size)
-console.log(myMap.has("role"))
+console.log(myMap);
+console.log(myMap.size);
+console.log(myMap.has("role"));
 
 // Needed Output
 // Map(3) {'username' => 'Osama', 'role' => 'Admin', 'country' => 'Egypt'}
 // 3
 // true
+
+//////////////////////////
+
+let theNumber = 100020003000;
+console.log(+[...new Set(theNumber.toString())].sort().join(""));
+
+// Needed Output
+// 123
+
+//////////////////////////
+
+let theName = "Elzero";
+console.log(Array.from(theName));
+console.log([...theName]);
+console.log(theName.split(""));
+
+// Needed Output
+// ['E', 'l', 'z', 'e', 'r', 'o']
+
+//////////////////////////
+
+let chars = ["A", "B", "C", "D", "E", 10, 15, 6];
+
+for (let i = 0; i < chars.length; i++) {
+  if (typeof chars[i] === "number") {
+    chars.unshift(...chars.splice(i, 1));
+  }
+}
+let fristIndexChar = chars.findIndex((e) => typeof e === "string");
+
+chars.copyWithin(0, fristIndexChar);
+console.log(chars);
+
+// Needed Output
+// ['A', 'B', 'C', 'A', 'B', 'C', 'D', 'E']
+
+//////////////////////////
+
+let numsOne = [1, 2, 3];
+let numsTwo = [4, 5, 6];
+
+console.log([...numsOne, ...numsTwo]);
+console.log(numsOne.concat(numsTwo));
+console.log(Array.from([...numsOne, ...numsTwo]));
+
+// Needed Output
+// [1, 2, 3, 4, 5, 6]
+
+//////////////////////////
+
+let n1 = [10, 30, 10, 20];
+let n2 = [30, 20, 10];
+
+console.log([...n1, ...n2].length * Math.max(...n1)); // 210
+
+// Needed Output
+// 210
